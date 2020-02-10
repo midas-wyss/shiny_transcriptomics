3410186df032558dc6a604ba96d213b0e44e438a
Rani Powers <rani.schwindt@gmail.com>
2020-02-09 20:27:02 -0500

    Styling

--- app_ui.R
+++ app_ui.R
@@ -93,10 +93,11 @@ AuthenticatedUI <- dashboardPage(
                    style = 'color: #42B5BB;'), 
         style = 'font-size: 8pt; margin: 0px 5px 20px 0px;'),
     sidebarMenu(
-      menuItem("Samples", tabName = "samples", icon = icon("vial")),
-      menuItem("Differential expression", tabName = "diff_expr", icon = icon("chart-bar")),
-      menuItem("Biomarkers", tabName = "biomarkers", icon = icon("share-alt")),
-      menuItem("DRUID", tabName = "druid", icon = icon("star"))
+      menuItem("Samples", tabName = "tab_samples", icon = icon("vial")),
+      menuItem("Differential expression", tabName = "tab_diff_expr", icon = icon("chart-bar")),
+      menuItem("Pathway enrichment", tabName = "tab_pathway_enrichment", icon = icon("align-left")),
+      menuItem("Biomarkers", tabName = "tab_biomarkers", icon = icon("share-alt")),
+      menuItem("DRUID", tabName = "tab_druid", icon = icon("star"))
     ),
     br(),
     div(p('This tool is maintained by the Predictive BioAnalytics group - Wyss Institute at Harvard',
@@ -109,48 +110,108 @@ AuthenticatedUI <- dashboardPage(
   
   dashboardBody(
     tags$head(includeHTML('www/analytics.html'),
+              tags$style(".shiny-output-error{color: white;}"),
               HTML('<link rel="icon" href="favicon.ico" type="image/x-icon"/>')),
     tabItems(
-      tabItem(tabName = "samples",
+      tabItem(tabName = "tab_samples",
               fluidRow(
                 box(title = "Between-sample variability (UMAP algorithm)", 
                     width = 7,
                     height = 500,
+                    p(style='padding-left: 20px; color: #D2D6DD;', 'Hover over points to view sample info'),
                     div(withSpinner(plotlyOutput('umap_plot'),
                                     type = 4, color = '#42B5BB'),
                         style = "overflow-y: auto;")),
                 box(title = "UMAP plot settings",
                     width = 5,
                     height = 500,
-                    div(style = 'padding-left: 20px; float: left;',
+                    div(style = 'padding: 0 30px 20px 30px; float: left;',
                         radioButtons('umap_color_by', 
                                      label = 'Color samples by',
                                      choices = 'Loading...',
                                      selected = character(0))),
-                    div(style = 'padding-left: 30px; float: left;',
+                    div(style = 'padding-bottom: 20px;',
                         radioButtons('umap_shape_by', 
                                      label = 'Shape samples by',
                                      choices = 'Loading...',
                                      selected = character(0))
                     ),
-                    br(),
-                    div(style = 'position: relative; padding: 20px;',
-                        hr()),
-                    downloadButton('download_umap_pdf', 'Download plot (PDF)')
+                    div(style = 'padding-top: 80px; padding-left: 20px;',
+                        downloadButton('download_umap_pdf', 'Download plot (PDF)',
+                                   style = 'color: #ffffff; background-color: #42B5BB; border-color: #38a1a6;
+    border-radius: 5px;')
+                    )
                   )
               ),
               fluidRow(
-                box(title = "All sample meta-data",
+                box(title = "Sample metadata",
+                    div(style='padding-left: 20px; padding-bottom: 10px;', 
+                        uiOutput('info_samples')),
                     width = 12,
                     div(style = 'padding-left: 20px; overflow-y: auto; height: 600px;',
-                        withSpinner(dataTableOutput('sample_metadata'),
+                        withSpinner(dataTableOutput('table_sample_metadata'),
                                     type = 4, color = '#42B5BB'))
                 )
               )
       ),
       
-      tabItem(tabName = "diff_expr",
+      tabItem(tabName = "tab_diff_expr",
+              fluidRow(
+                box(title = "Differential expression analysis parameters", 
+                    width = 5,
+                    height = 500,
+                    div(style = 'padding-left: 20px;',
+                        selectInput('select_volcano_column', 'Split groups by',
+                                        'Loading...', selected = character(0)),
+                        div(style = 'float: left;',
+                            textInput('text_group1', 'Group A Name', 'Group A'),
+                            selectInput('select_group1_criteria', 'Group A criteria',
+                                        'Loading...', selected = character(0))
+                            
+                        ),
+                        div(style = 'float: left; padding-left: 20px;',
+                            textInput('text_group2', 
+                                      label = 'Group B Name', 'Group B'),
+                            selectInput('select_group2_criteria', 'Group B criteria',
+                                        'Loading...', selected = character(0))
+                        ),
+                        div(style = 'padding-bottom: 20px;',
+                            actionButton('button_run_volcano', 'Run analysis',
+                                         icon = icon("angle-double-right"),
+                                         style = 'color: #ffffff; background-color: #42B5BB; border-color: #38a1a6;
+    border-radius: 5px;')),
+                        div(downloadButton('download_volcano_pdf', 'Download plot (PDF)',
+                                       style = 'color: #ffffff; background-color: #42B5BB; border-color: #38a1a6;
+    border-radius: 5px;'))
+                    )
+                ),
+                box(title = "Volcano plot",
+                    width = 7,
+                    height = 500,
+                    div(withSpinner(plotlyOutput('volcano_plot'),
+                                    type = 4, color = '#42B5BB'))
+                )
+              ),
+              fluidRow(
+                box(title = "Differentially expressed genes",
+                    width = 12,
+                    div(style = 'padding-left: 20px; overflow-y: auto; height: 600px;',
+                        withSpinner(dataTableOutput('table_differential_expression'),
+                                    type = 4, color = '#42B5BB'))
+                )
+              )
+      ),
+      
+      tabItem(tabName = "tab_pathway_enrichment",
               h2("Plots tab content")
+      ),
+      
+      tabItem(tabName = "tab_biomarkers",
+              h2("Coming soon!")
+      ),
+      
+      tabItem(tabName = "tab_druid",
+              h2("Coming soon!")
       )
     )
   )
